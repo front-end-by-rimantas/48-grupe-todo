3c84330dc3bc78db5acf902111e7190f7afa5004
Rimantas <belauzas@gmail.com>
2024-02-20 11:08:33 +0200

    Initial data

--- js/data/data.js
+++ js/data/data.js
@@ -1,4 +1,44 @@
 const data = {
+    tasks: [
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia jog butu slapia ir silta',
+            status: 'done',
+        },
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia jog butu slapia ir silta',
+            status: 'in-progress',
+        },
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia jog butu slapia ir silta',
+            status: 'in-progress',
+        },
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia jog butu slapia ir silta',
+            status: 'todo',
+        },
+    ],
+    columns: [
+        {
+            title: 'Task ready',
+            status: 'todo',
+        },
+        {
+            title: 'In progress',
+            status: 'in-progress',
+        },
+        {
+            title: 'Needs review',
+            status: 'review',
+        },
+        {
+            title: 'Done',
+            status: 'done',
+        },
+    ],
 };
 
 export { data };
