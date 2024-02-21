1bc133ca818ac699e2391fa55a5978e60513da30
Rimantas <belauzas@gmail.com>
2024-02-21 11:13:31 +0200

    Table

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,20 +1,14 @@
 import { Table } from "../components/Table.js";
 
 const todo = new Table(
+    '',
     'Homepage design',
     'Panašu, jog dar neturi jokių užduočių'
 );
 
-const todo2 = new Table(
-    'Web development tasks',
-    'Nėra sukurta jokių užduočių. Susikurk!'
-);
+todo.addColumn('Backlog');
+todo.addColumn('In progress');
+todo.addColumn('Review');
+todo.addColumn('Done');
 
 console.log(todo);
-console.log(todo2);
-
-console.log(todo.labas('Lietuva'));
-console.log(todo2.labas('Latvija'));
-
-console.log(todo.labanakt('vaikučiams'));
-console.log(todo2.labanakt('suaugusiems'));
